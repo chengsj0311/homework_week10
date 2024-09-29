482ba2f6db5f50df75a9bf37659e18b2f3fab98d
SHENG JIE CHENG <113522864+chengsj0311@users.noreply.github.com>
2024-09-29 23:33:28 +0800

    Add files via upload

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "../js/map.vg.json";
+vegaEmbed('#vis2', vg_1).then(result => {}).catch(console.error);
+
+var vg_2 = "../js/bar.vg.json";
+vegaEmbed('#vis1', vg_2).then(result => {}).catch(console.error);
